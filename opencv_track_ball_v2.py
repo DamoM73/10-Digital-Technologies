2f98b9b925bac10bad2569df9141bb6df6edccd1
DamoM73 <damomurtagh@gmail.com>
2020-11-10 16:22:26 +1000

    Ball track v2

--- opencv_track_ball_v2.py
+++ opencv_track_ball_v2.py
@@ -1,23 +1,13 @@
 # import necessary packages
-from collections import deque
 from imutils.video import VideoStream
 import numpy as np
-import argparse
 import cv2
 import imutils
 import time
 
-# construct the arguement parse and parse the arguements
-ap = argparse.ArgumentParser()
-#ap.add_argument('-v', '--video', help='path to the (optional) video file')
-args = vars(ap.parse_args())
-buffer = 64
-
 # define the lower and upper boundaries of the ball in the HSV color space
-# initialize the lsit of tracked points
 blue_lower = (89,138,50)
 blue_upper = (125,255,164)
-pts = deque(maxlen=buffer)
 
 # grab the reference to the webcam
 vs = VideoStream(src=0, usePiCamera=True).start()
@@ -29,9 +19,6 @@ time.sleep(0.5)
 while True:
     # grab the current frame
     frame = vs.read()
-    
-    # handle the frame from VideoCapture or Video Stream
-    #frame = frame[1] if args.get("video", False) else frame
     
     # resize the frame, blur it flip it, and convert it to the HSV color space
     frame = cv2.flip(frame, -1)
@@ -56,7 +43,6 @@ while True:
         # the minimum enclosing circle and centroid
         c = max(cents, key=cv2.contourArea)
         ((x,y), radius) = cv2.minEnclosingCircle(c)
-        #print(f"{x},{y},{radius}")
         M = cv2.moments(c)
         centre = (int(M["m10"] / M["m00"]), int(M["m01"]/M["m00"]))
         
@@ -65,19 +51,7 @@ while True:
             # draw the circle and centroid on the frame, then update the list of the tracked points
             cv2.circle(frame, (int(x), int(y)), int(radius),(0,255,255),2)
             cv2.circle(frame, center, 5, (0,0,255), -1)
-    # update the points queue
-    pts.appendleft(center)
-    
-    # loop over the set of tracked points
-    for i in range(1, len(pts)):
-        # if either of the tracked points are None, ignore
-        if pts[i - 1] is None or pts[i] is None:
-            continue
-        
-        # otherwise, compute the thickness of the line and draw the connecting lines
-        thickness = int(np.sqrt(buffer/ float(i + 1)) * 2.5)
-        cv.line(frame, pts[i-1],pts[i], (0,0,255), thickness)
-        
+      
     # show frame to our screen
     cv2.imshow("Frame", frame)
     key = cv2.waitKey(1) & 0xFF
