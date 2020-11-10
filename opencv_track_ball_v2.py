f917a01f5cbd297bed32d0309dc87b8005b9226c
DamoM73 <damomurtagh@gmail.com>
2020-11-10 15:11:24 +1000

    Ball tracking complete

--- opencv_track_ball_v2.py
+++ opencv_track_ball_v2.py
@@ -9,22 +9,18 @@ import time
 
 # construct the arguement parse and parse the arguements
 ap = argparse.ArgumentParser()
-ap.add_argument('-v', '--video', help='path to the (optional) video file')
-ap.add_argument('-b', '--buffer', type=int, default=64, help='max buffer size')
+#ap.add_argument('-v', '--video', help='path to the (optional) video file')
 args = vars(ap.parse_args())
+buffer = 64
 
 # define the lower and upper boundaries of the ball in the HSV color space
 # initialize the lsit of tracked points
 blue_lower = (89,138,50)
 blue_upper = (125,255,164)
-pts = deque(maxlen=args["buffer"])
+pts = deque(maxlen=buffer)
 
-# if a video path was not supplied, grab the reference to the webcam
-if not args.get("video", False):
-    vs = VideoStream(src=0, usePiCamera=True).start()
-# otherwise, grab a reference to the video file
-else:
-    vs = cv2.VideoCapture(args["video"])
+# grab the reference to the webcam
+vs = VideoStream(src=0, usePiCamera=True).start()
     
 # allow the camera or video file to intialise
 time.sleep(0.5)
@@ -35,11 +31,7 @@ while True:
     frame = vs.read()
     
     # handle the frame from VideoCapture or Video Stream
-    frame = frame[1] if args.get("video", False) else frame
-    
-    # if we are viewing video and we did not grab a frame, then we have readed the end of the video
-    if frame is None:
-        break
+    #frame = frame[1] if args.get("video", False) else frame
     
     # resize the frame, blur it flip it, and convert it to the HSV color space
     frame = cv2.flip(frame, -1)
@@ -64,10 +56,11 @@ while True:
         # the minimum enclosing circle and centroid
         c = max(cents, key=cv2.contourArea)
         ((x,y), radius) = cv2.minEnclosingCircle(c)
+        #print(f"{x},{y},{radius}")
         M = cv2.moments(c)
         centre = (int(M["m10"] / M["m00"]), int(M["m01"]/M["m00"]))
         
-        # only proceed if the radius meets minimu size
+        # only proceed if the radius meets minimum size
         if radius > 10:
             # draw the circle and centroid on the frame, then update the list of the tracked points
             cv2.circle(frame, (int(x), int(y)), int(radius),(0,255,255),2)
@@ -82,7 +75,7 @@ while True:
             continue
         
         # otherwise, compute the thickness of the line and draw the connecting lines
-        thickness = int(np.sqrt(arge["buffer"] / float(i + 1)) * 2.5)
+        thickness = int(np.sqrt(buffer/ float(i + 1)) * 2.5)
         cv.line(frame, pts[i-1],pts[i], (0,0,255), thickness)
         
     # show frame to our screen
@@ -92,15 +85,10 @@ while True:
     if key == ord("q"):
         break
     
-# if we are not using a video file, stop the camera video stream
-if not args.get("video", False):
-    vs.stop()
-    
-# otherwise release the camera
-else:
-    vs.release()
+# release the camera
+vs.stop()
     
 # close all windows
-cv2.destroyAlWlWindows()
+cv2.destroyAllWindows()
     
     
